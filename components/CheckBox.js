e0076ab7200a7b646999b0e73ec1536d1302cba7
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-09-28 14:12:26 +0800

    added checkbox component

--- components/CheckBox.js
+++ components/CheckBox.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
 
 class CheckBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isReady: false,
+        };
+
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
+    onChangeHandler = e => {
+        this.setState({ [e.target.name]: e.target.checked }); 
+    }
     render() {
         return (
-            <h1> this is my CheckBox component </ h1>
+            <>
+                <h5> step 3</h5>
+                <form>
+                    <label>
+                        are you ready to rock?
+                        <input
+                            type="checkbox"
+                            name="isReady"
+                            checked={this.isReady}
+                            onChange={this.onChangeHandler}>
+                        </input>
+                    </label>
+                    <button
+                        id="completeRegistertion"
+                        type="submit"
+                        className="complete_registertion"
+                        disabled={this.state.isReady != true}>
+                        Complete Registertion
+                    </button>
+                </form>
+            </>
         )
     }
 }
